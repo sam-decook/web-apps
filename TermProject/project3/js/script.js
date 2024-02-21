d53ee99390a77685bfc5502779b5d2b6ae068b10
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2024-02-21 18:57:58 -0500

    handoff

--- TermProject/project3/js/script.js
+++ TermProject/project3/js/script.js
@@ -454,8 +454,8 @@ let requirements = {
 //     return response.json();
 //   })
 //   .then(data => {
-//     plan = data.plan;
-//     catalog = data.catalog;
+//     let = plan = data.plan;
+//     let = catalog = data.catalog;
 //   })
 //   .catch(error => console.error("Request failed: ", error));
 
@@ -467,204 +467,29 @@ let requirements = {
 //     return response.json();
 //   })
 //   .then(data => {
-//     requirements = data
+//     let = requirements = data
 //   })
 //   .catch(error => console.error("Request failed: ", error));
-
-
-// let plan = {
-//   name: "Whatever you want really",
-//   catalogYear: "2020-2021",
-//   major: "Bachelor of Science in Computer Engineering",
-//   minor: "Bible",
-//   studentName: "John Doe",
-//   currentSemester: "Fall 2022",
-//   courses: [
-//     {
-//       term: "Spring",
-//       year: 2024,
-//       courseDesignator: "CS-3510",
-//       courseName: "Compiler Theory & Practice",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2023,
-//       courseDesignator: "EGCP-2200",
-//       courseName: "Digital Systems",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2020,
-//       courseDesignator: "MATH-1710",
-//       courseName: "Calculus I",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2021,
-//       courseDesignator: "EGCP-3400",
-//       courseName: "Microcontrollers",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2021,
-//       courseDesignator: "EGCP-2200",
-//       courseName: "Embedded Systems",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2023,
-//       courseDesignator: "EGGN-4010",
-//       courseName: "Senior Seminar",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2022,
-//       courseDesignator: "MATH-3220",
-//       courseName: "Discrete Mathematics",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2020,
-//       courseDesignator: "EGCP-1010",
-//       courseName: "Digital Logic Design",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2020,
-//       courseDesignator: "CS-1210",
-//       courseName: "C++ Programming",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2021,
-//       courseDesignator: "PHYS-2110",
-//       courseName: "Physics I",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2021,
-//       courseDesignator: "MATH-1720",
-//       courseName: "Calculus II",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2021,
-//       courseDesignator: "CS-1220",
-//       courseName: "Object Oriented Design with C++",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2021,
-//       courseDesignator: "PHYS-2120",
-//       courseName: "Physics II",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2021,
-//       courseDesignator: "CS-2210",
-//       courseName: "Data Structures using JAVA",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2022,
-//       courseDesignator: "EGCP-3210",
-//       courseName: "Computer Architecture",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2022,
-//       courseDesignator: "CS-3310",
-//       courseName: "Operating Systems",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2022,
-//       courseDesignator: "CS-3410",
-//       courseName: "Algorithims",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2022,
-//       courseDesignator: "CS-4710",
-//       courseName: "Computer Graphics",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2022,
-//       courseDesignator: "COM-1100",
-//       courseName: "Speech",
-//     },
-//     {
-//       term: "Summer",
-//       year: 2022,
-//       courseDesignator: "ENG-1400",
-//       courseName: "Composition",
-//     },
-//     {
-//       term: "Summer",
-//       year: 2023,
-//       courseDesignator: "BTGE-3765",
-//       courseName: "Theology II",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2023,
-//       courseDesignator: "EGCP-3010",
-//       courseName: "Advanced Digital Logic Design",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2023,
-//       courseDesignator: "CS-3320",
-//       courseName: "Linux System Programming",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2023,
-//       courseDesignator: "EGCP-4810",
-//       courseName: "Senior Design",
-//     },
-//     {
-//       term: "Fall",
-//       year: 2023,
-//       courseDesignator: "EGCP-4310",
-//       courseName: "Computer Networks",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2024,
-//       courseDesignator: "CS-3220",
-//       courseName: "Web Apps",
-//     },
-//     {
-//       term: "Spring",
-//       year: 2024,
-//       courseDesignator: "CS-3610",
-//       courseName: "Database Organization and Design",
-//     },
-//   ],
-// };
 
 // Give each course a unique ID for drag and drop
 let courseId = 0;
 
 function renderPlan() {
-  let elem = document.querySelector(".plan-grid");
+  let $elem = $(".plan-grid");
 
-  let organizedCourses = sortCourses(plan.courses);
+  // let organizedCourses = sortCourses(plan.courses);
+  let courses = organizeCourses(plan);
+  console.log("Organized courses: " + courses);
 
-  Object.keys(organizedCourses).forEach((year) => {
+  Object.keys(courses).forEach(year => {
     Object.keys(organizedCourses[year]).forEach((term) => {
       let trueYear = Number(year);
       if (term != "Fall") {
         trueYear += 1;
       }
 
-      let semesterDiv = document.createElement("div");
-      semesterDiv.classList.add("semester");
-      semesterDiv.setAttribute("ondragover", "onDragOver(event);");
-      semesterDiv.setAttribute("ondragleave", "onDragLeave(event);");
-      semesterDiv.setAttribute("ondrop", "onDrop(event);");
+      let $div = $(`<div class="semester" ondragover="onDragOver(event);" ondragleave="onDragLeave(event);" ondrop="onDrop(event);"></div>`);
+
       semesterDiv.id = `${term.toLowerCase()}${trueYear}`;
 
       const heading = document.createElement("h3");
@@ -672,52 +497,105 @@ function renderPlan() {
       semesterDiv.appendChild(heading);
 
       organizedCourses[year][term].forEach((course) => {
-        let courseParagraph = document.createElement("p");
-        courseParagraph.setAttribute("draggable", "true");
-        courseParagraph.setAttribute("ondragstart", "onDragStart(event);");
-        courseParagraph.setAttribute("id", String(courseId));
+        let desig = course.courseDesignator;
+        let name = course.courseName;
+        let $course = $(`<p id="${id}" draggable="true" ondragstart="onDragStart(event)"><span class="tag">${desig}</span> ${name}</p>`);
         courseId += 1;
-
-        let courseSpan = document.createElement("span");
-        courseSpan.classList.add("tag");
-        courseSpan.textContent = course.courseDesignator;
-
-        courseParagraph.appendChild(courseSpan);
-        courseParagraph.append(` ${course.courseName}`);
-
-        semesterDiv.appendChild(courseParagraph);
+        $div.append($course)
       });
 
-      elem.appendChild(semesterDiv);
+      $elem.appendChild(semesterDiv);
     });
   });
 }
 
-//Course[{}] => Year[Term[Course[{}]]]
-function sortCourses(courseObj) {
-  let organizedCourses = {};
-  const terms = ["Fall", "Spring", "Summer"];
+function organizeCourses(plan) {
+  let courses = {};
+  let terms = ["Fall", "Spring", "Summer"];
 
-  courseObj.forEach(course => {
+  Object.keys(plan.courses).forEach(course => {
     let year = course.year;
     if (course.term != "Fall") {
       year -= 1;
     }
 
-    if (!organizedCourses[year]) {
-      organizedCourses[year] = {};
-      terms.forEach((term) => {
-        organizedCourses[year][term] = [];
+    if (!courses[year]) {
+      courses[year] = {};
+      terms.forEach(term => {
+        courses[year][term] = [];
       });
     }
 
-    organizedCourses[year][course.term].push({
-      courseDesignator: course.courseDesignator,
-      courseName: course.courseName,
-    });
-  });
+    courses[year][course.term].push(course);
 
-  return organizedCourses;
+    return courses;
+  });
 }
 
+$(document).ready(function(){
+  
+  function populateCourseFinder(){
+
+  const $courseSearch = $("#courseSearch");  
+  $courseSearch.empty();
+
+  if(courseId.match(/[A-Z]{2,4}-\d{4}/)){
+    addCourseToTable(, courseId, catalog.courses[courseId].name, catalog.courses[courseId].description);
+  }
+  
+  $.each(catalog.courses, function(courseId, courseName){
+  
+    $('<option>').val(`${courseId} ${courseName}`).appendTo($courseSearch);
+    
+  });
+
+  const totalEntries = Object.keys(catalog.courses).length;
+  const totalEntriesText = `Showing 1 to ${totalEntries} of ${totalEntries} entries`;
+  $('.course-finder p').text(totalEntriesText);
+  }
+
+  populateCourseFinder();
+
+  function addCourseToTable(credits, id, name, description) {
+    var newRow = $('<tr></tr>');
+    newRow.append($('<td></td>').text(credits));
+    newRow.append($('<td></td>').text(id));
+    newRow.append($('<td></td>').text(name));
+    newRow.append($('<td></td>').text(description));
+    $('#coursesTable').append(newRow); 
+  }
+  
+  
+
+
+});
+
 document.addEventListener("DOMContentLoaded", renderPlan);
+
+async function fetchcourses(){
+  try{
+    const response = await fetch("getCombined.json");
+    if (!response.ok){
+      throw new Error("Failed to fetch");
+    }
+    const data = await response.json();
+    return data.courses;
+  }catch (error){
+    console.error(error);
+  }
+}
+
+
+async function populateTable(){
+  const courses = await fetchcourses();
+  const table = document.getElementById("coursesTable");
+  for (const courseId in courses){
+    const course = courses[courseId];
+    const row = table.insertRow();
+    row.innerHTML = '<td>${course.credits}</td><td>${course.id}</td><td>${course.name}</td><td>${course.description}</td>';
+  }
+}
+
+
+
+
