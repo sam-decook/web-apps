a7d022c235a0d2009c2ea227396e45183a3edc3d
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2024-03-14 15:12:47 -0400

    moving to judah

--- TermProject/project4/login.php
+++ TermProject/project4/login.php
@@ -1,40 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>APE Login Page</title>
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-</head>
-
-<body>
-    <div class="login-container">
-        <h2>Welcome to the Academic Planning Environment</h2>
-        <form id="login-form" action="login.php" method="POST">
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="text" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div class="form-group">
-                <button class="btn" type="submit">Login</button>
-            </div>
-        </form>
-    </div>
-</body>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>APE Login Page</title>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/login.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="login-container">
+            <h2>Welcome to the Academic Planning Environment</h2>
+            <form id="login-form" action="login.php" method="POST">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="text" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <div class="form-group">
+                    <button class="btn" type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+    </body>
 </html>
 <?php
-session_start();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $server = "james.cedarville.edu";
     $username = "cs3220_sp24";
@@ -58,6 +53,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $row = $result->fetch_assoc();
 
         if (password_verify($_POST["password"], $row["password"])) {
+            session_start();
             $_SESSION["email"] = $row["email"];
             header("Location: ape.php");
             $conn->close();
